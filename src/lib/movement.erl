5399495e87afa8bd97652d3e8848f930f5130c12
Jamie Clinton <jcclinton24@gmail.com>
2014-07-09 09:27:23 -0700

    added code to verify map coords when moving

--- src/lib/movement.erl
+++ src/lib/movement.erl
@@ -6,6 +6,11 @@
 -include("include/binary.hrl").
 
 
+-define(MAX_NUMBER_OF_GRIDS, 64).
+-define(SIZE_OF_GRIDS, 533.33333).
+-define(MAP_SIZE, ?SIZE_OF_GRIDS*?MAX_NUMBER_OF_GRIDS).
+-define(MAP_HALFSIZE, ?MAP_SIZE/2).
+
 
 move_fall_land(PropList) ->
 	io:format("received req to move time land~n"),
@@ -28,6 +33,8 @@ handle_movement(PropList) ->
 	Opcode = opcode_patterns:getNumByAtom(msg_move_start_forward),
 	Payload = proplists:get_value(payload, PropList),
 	<<MoveFlags?L, Time?L, X?f, Y?f, Z?f, O?f, Unk1?L>> = Payload,
+	Allowable = verify_movement(X, Y, Z, O),
+	io:format("allowable: ~p~n", [Allowable]),
 	%Msg = <<Opcode?W, Payload/binary>>,
 	%io:format("moveflags: ~p~ntime: ~p~nopcode: ~p~npayload: ~p~n", [MoveFlags, Time, Opcode, Payload]),
 	io:format("moveflags: ~p pos: {~p,~p,~p,~p}~n", [MoveFlags, X, Y, Z, O]),
@@ -35,3 +42,26 @@ handle_movement(PropList) ->
 	ok.
 
 null() -> ok.
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%
+%% private
+
+verify_movement(X, Y, Z, O) ->
+	finite(O) andalso verify_movement(X, Y, Z).
+
+verify_movement(X, Y, Z) ->
+	finite(Z) andalso verify_movement(X, Y).
+
+verify_movement(X, Y) ->
+	verify_movement(X) andalso verify_movement(X).
+
+verify_movement(C) ->
+	finite(C) andalso (abs(C) =< ?MAP_HALFSIZE - 0.5).
+	
+
+
+
+finite(_) -> true.
