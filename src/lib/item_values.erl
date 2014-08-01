f3f428a0e389dc37aa3d58f9f4ec16f20b0b5808
Jamie Clinton <jcclinton24@gmail.com>
2014-08-01 10:27:56 -0700

    added some sets and gets to item_values module

--- src/lib/item_values.erl
+++ src/lib/item_values.erl
@@ -1,33 +1,35 @@
 -module(item_values).
 
 -export([get_guid/1, get_item_id/1]).
+-export([get_owner/1, get_contained/1]).
+-export([set_guid/2, set_item_id/2]).
 -export([set_contained/2, set_owner/2]).
 
+% gets
 
 get_guid(Values) ->
-	object_values:get_uint64_value( 'OBJECT_FIELD_GUID', Values).
+	object_values:get_uint64_value('OBJECT_FIELD_GUID', Values).
 
 get_item_id(Values) ->
-	object_values:get_uint32_value( 'OBJECT_FIELD_ENTRY', Values).
+	object_values:get_uint32_value('OBJECT_FIELD_ENTRY', Values).
 
+get_owner(Values) ->
+	object_values:get_uint64_value('ITEM_FIELD_OWNER', Values).
+
+get_contained(Values) ->
+	object_values:get_uint64_value('ITEM_FIELD_CONTAINED', Values).
+
+
+% sets
+
+set_guid(Value, Values) ->
+	object_values:set_uint64_value('OBJECT_FIELD_GUID', Value, Values).
+
+set_item_id(Value, Values) ->
+	object_values:set_uint32_value('OBJECT_FIELD_ENTRY', Value, Values).
 
 set_owner(Value, Values) ->
-	Field = 'ITEM_FIELD_OWNER',
-	set_uint64_mark_if_needed(Field, Value, Values).
+	object_values:set_uint64_value('ITEM_FIELD_OWNER', Value, Values).
 
 set_contained(Value, Values) ->
-	Field = 'ITEM_FIELD_CONTAINED',
-	set_uint64_mark_if_needed(Field, Value, Values).
-
-
-set_uint64_mark_if_needed(Field, NewValue, Values) ->
-	Value = object_values:get_uint64_value(Field, Values),
-	if Value /= NewValue ->
-			mark_update(Field, Values),
-			object_values:set_uint64_value(Field, NewValue, Values);
-		true -> Values
-	end.
-
-mark_update(_Field, _Values) ->
-	% todo implement
-	ok.
+	object_values:set_uint64_value('ITEM_FIELD_CONTAINED', Value, Values).
