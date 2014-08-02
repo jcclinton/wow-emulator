b3000b31ffb0ebc0920861fd5667a5e058cb6fc7
Jamie Clinton <jcclinton24@gmail.com>
2014-08-01 18:04:47 -0700

    am now starting combat on an attack swing

--- src/handlers/combat.erl
+++ src/handlers/combat.erl
@@ -6,9 +6,20 @@
 
 
 attack_swing(Data) ->
+	Guid = recv_data:get(guid, Data),
 	<<TargetGuid?Q>> = recv_data:get(payload, Data),
 	io:format("attack swing~n"),
+
+	StartPayload = <<Guid?Q, TargetGuid?Q>>,
+	OpAtom = smsg_attackstart,
+	world:send_to_all(OpAtom, StartPayload),
 	ok.
+
+	%error case
+	%PackGuid = <<16#FF?B, Guid?Q>>,
+	%TargetPackGuid = <<16#FF?B, TargetGuid?Q>>,
+	%StopPayload = <<PackGuid/binary, TargetPackGuid/binary, 0?L>>,
+	%{smsg_attackstop, StopPayload}.
 
 attack_stop(_Data) ->
 	% payload is empty
