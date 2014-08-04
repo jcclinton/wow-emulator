5f9d7c20e0914dd56332d395d2463fef3039c0f0
Jamie Clinton <jcclinton24@gmail.com>
2014-08-03 19:18:28 -0700

    cleaned up some spell code

--- src/handlers/spell.erl
+++ src/handlers/spell.erl
@@ -11,16 +11,18 @@ cast(Data) ->
 	Payload = recv_data:get(payload, Data),
 	<<SpellId?L, Targets/binary>> = Payload,
 	io:format("cast id ~p with targets ~p~n", [SpellId, Targets]),
-	PackGuid = <<16#FF?B, Guid?Q>>,
+
+	PackGuid = guid:pack(Guid),
 	CastFlag = ?cast_flag_unknown9,
 	OutPayload = <<PackGuid/binary, PackGuid/binary, SpellId?L, CastFlag?W, 1?B, 0?B, Targets/binary>>,
 	OpAtom = smsg_spell_go,
-	world:send_to_all(OpAtom, OutPayload),
-	Error = ?spell_failed_immune,
+	%world:send_to_all(OpAtom, OutPayload),
+
 	{smsg_cast_failed, <<SpellId?L, 0?B>>}.
 
 
 cancel_cast(Data) ->
 	<<SpellId?L>> = recv_data:get(payload, Data),
 	io:format("cancel spell id: ~p~n", [SpellId]),
-	ok.
+	Error = ?spell_failed_immune,
+	{smsg_cast_failed, <<SpellId?L, Error?B>>}.
