16a35d82331c5d8401fcc9a9ff9255e1568e1387
Jamie Clinton <jcclinton24@gmail.com>
2014-09-04 14:08:10 -0700

    got item_values unit test working with new values system

--- test/item_values_tests.erl
+++ test/item_values_tests.erl
@@ -34,13 +34,9 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 object_values_test_() ->
-	[{"get set test",
+	[
+	{"get set test",
 	 {setup, fun init_values/0, fun stop/1, fun get_set_test/1}},
-
-	{"underflow value test",
-	 {setup, fun init_underflow/0, fun stop/1, fun throw_test/1}},
-	{"overflow value test",
-	 {setup, fun init_overflow/0, fun stop/1, fun throw_test/1}},
 
 	{"create values test",
 	 {setup, fun init_create/0, fun stop/1, fun create_test/1}}
@@ -67,22 +63,9 @@ init_values() ->
 	Values = get_empty_values(),
 	Value32 = 16#1010BABA,
 	Value64 = 16#EFEF5678ACAC1234,
-	Funs = get_funs(),
-	{Values, Value32, Value64, Funs}.
+	Fields = get_fields(),
+	{Values, Value32, Value64, Fields}.
 
-init_overflow() ->
-	Values = get_empty_values(),
-	Value32 = 16#FFFFFFFF + 1,
-	Value64 = 16#FFFFFFFFFFFFFFFF + 1,
-	Funs = get_funs(),
-	{Values, Value32, Value64, Funs}.
-
-init_underflow() ->
-	Values = get_empty_values(),
-	Value32 = -1,
-	Value64 = -1,
-	Funs = get_funs(),
-	{Values, Value32, Value64, Funs}.
 
 
 stop(_SetupData) ->
@@ -97,15 +80,15 @@ stop(_SetupData) ->
 create_test({ItemGuid, ItemId, OwnerGuid}) ->
 	Values = item_values:create(ItemGuid, ItemId, OwnerGuid),
 
-	GetGuid = item_values:get_guid(Values),
+	GetGuid = item_values:get_value(object_field_guid, Values),
 
-	GetItemId = item_values:get_item_id(Values),
+	GetItemId = item_values:get_value(object_field_entry, Values),
 
-	GetStackCount = item_values:get_stack_count(Values),
+	GetStackCount = item_values:get_value(item_field_stack_count, Values),
 
-	GetOwner = item_values:get_owner(Values),
+	GetOwner = item_values:get_value(item_field_owner, Values),
 
-	GetContained = item_values:get_contained(Values),
+	GetContained = item_values:get_value(item_field_contained, Values),
 
 	[
 		?_assertEqual(ItemGuid, GetGuid),
@@ -118,88 +101,60 @@ create_test({ItemGuid, ItemId, OwnerGuid}) ->
 
 
 
-get_set_test({Values, Value32, Value64, Funs}) ->
+get_set_test({Values, Value32, Value64, Fields}) ->
 
 	% test that any values are getting changed
 
-	SetFuns = proplists:get_value(sets, Funs),
-	SetFuns32 = proplists:get_value(32, SetFuns),
-	SetFuns64 = proplists:get_value(64, SetFuns),
+	WordFields = proplists:get_value(32, Fields),
+	QuadFields = proplists:get_value(64, Fields),
 
-	GetFuns = proplists:get_value(gets, Funs),
-	GetFuns32 = proplists:get_value(32, GetFuns),
-	GetFuns64 = proplists:get_value(64, GetFuns),
+
 
 	%sets
-	{T1, NewValues1, Count1} = lists:foldl(fun(SetFun, {TestAcc, ValuesAcc, Count}) ->
-		NewValuesInner = item_values:SetFun(Value64+Count, ValuesAcc),
+	{T1, NewValues1, Count1} = lists:foldl(fun(Field, {TestAcc, ValuesAcc, Count}) ->
+		NewValuesInner = item_values:set_value(Field, Value64+Count, ValuesAcc),
 		TestAccOut = [ ?_assertNotEqual(ValuesAcc, NewValuesInner) | TestAcc],
 		{TestAccOut, NewValuesInner, Count+1}
-	end, {[], Values, 0}, SetFuns64),
+	end, {[], Values, 0}, QuadFields),
 
-	{T2, NewValues, _} = lists:foldl(fun(SetFun, {TestAcc, ValuesAcc, Count}) ->
-		NewValuesInner = item_values:SetFun(Value32+Count, ValuesAcc),
+	{T2, NewValues, _} = lists:foldl(fun(Field, {TestAcc, ValuesAcc, Count}) ->
+		NewValuesInner = item_values:set_value(Field, Value32+Count, ValuesAcc),
 		TestAccOut = [ ?_assertNotEqual(ValuesAcc, NewValuesInner) | TestAcc],
 		{TestAccOut, NewValuesInner, Count+1}
-	end, {T1, NewValues1, Count1}, SetFuns32),
+	end, {T1, NewValues1, Count1}, WordFields),
 
 	%gets
-	{T3, Count2} = lists:foldl(fun(GetFun, {TestAcc, Count}) ->
-		Value = item_values:GetFun(NewValues),
+	{T3, Count2} = lists:foldl(fun(Field, {TestAcc, Count}) ->
+		Value = item_values:get_value(Field, NewValues),
 		TestAccOut = [ ?_assertEqual(Value64+Count, Value) | TestAcc],
 		{TestAccOut, Count+1}
-	end, {T2, 0}, GetFuns64),
+	end, {T2, 0}, QuadFields),
 
-	{T4, _} = lists:foldl(fun(GetFun, {TestAcc, Count}) ->
-		Value = item_values:GetFun(NewValues),
+	{T4, _} = lists:foldl(fun(Field, {TestAcc, Count}) ->
+		Value = item_values:get_value(Field, NewValues),
 		TestAccOut = [ ?_assertEqual(Value32+Count, Value) | TestAcc],
 		{TestAccOut, Count+1}
-	end, {T3, Count2}, GetFuns32),
+	end, {T3, Count2}, WordFields),
 
 	T4.
 
 
 
-throw_test({Values, Value32, Value64, Funs}) ->
-	SetFuns = proplists:get_value(sets, Funs),
-	SetFuns32 = proplists:get_value(32, SetFuns),
-	SetFuns64 = proplists:get_value(64, SetFuns),
-
-	T1 = lists:foldl(fun(SetFun, Acc) ->
-		[?_assertThrow(badarg, item_values:SetFun(Value64, Values) ) | Acc]
-	end, [], SetFuns64),
-
-	lists:foldl(fun(SetFun, Acc) ->
-		[?_assertThrow(badarg, item_values:SetFun(Value32, Values) ) | Acc]
-	end, T1, SetFuns32).
 
 
 %%%%%%%%%%%%
 % private
 get_empty_values() ->
-	TotalCount = update_fields:fields('ITEM_END'),
+	TotalCount = object_fields:get_total_count(item),
 	binary:copy(<<0?L>>, TotalCount).
 
 % put all gets and sets here
 % gets have to be in the same order as the sets
-get_funs() ->
+get_fields() ->
 	[
-		{sets,
-			[
-				{32,
-					[set_item_id, set_stack_count]},
-				{64,
-					[set_guid, set_owner, set_contained]
-				}
-			]
-		},
-		{gets,
-			[
-				{32,
-					[get_item_id, get_stack_count]},
-				{64,
-					[get_guid, get_owner, get_contained]
-				}
-			]
+		{32,
+			[object_field_entry, item_field_stack_count]},
+		{64,
+			[object_field_guid, item_field_owner, item_field_contained]
 		}
 	].
